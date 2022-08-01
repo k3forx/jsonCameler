aca7bc594393cacbd70fbf1337ee85595259f9cc
Kanata Miyahana <afiri.bupa@gmail.com>
2022-08-01 22:34:49 +0900

    add test case

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -3,10 +3,15 @@ package a
 import "time"
 
 type User struct {
-	UserID    int       `json:"userId"`    // OK
-	FirstName string    `json:"firstName"` // OK
-	LastName  string    `json:"-"`         // OK
-	Username  string    `json:"user-name"` // nocamel OK
+	UserID    int       `json:"userId"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"-"`
+	Username  string    `json:"user-name"` // nocamel
 	BirthDay  string    `json:"birth_day"` // want "invalid JSON tag `birth_day`"
 	CreatedAt time.Time `json:"_"`         // want "invalid JSON tag `_`"
+}
+
+type UserAddress struct {
+	ID      int
+	Country string `json:"country"`
 }
